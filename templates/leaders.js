4167ce679453fab97ae84252a1a8520ddc3cd2cf
Badger42S <57646489+Badger42S@users.noreply.github.com>
2021-02-18 08:24:10 -0500

    elements fabrica

--- templates/leaders.js
+++ templates/leaders.js
@@ -1,34 +1,49 @@
  const leadersTemplate = data => {
-    const mainContainer = document.createElement("div");
-    mainContainer.classList = "colCentre";
-    const boxContainer = document.createElement("div");
-    boxContainer.classList = "colCentre leaderBox";
-    const header = document.createElement("header");
-    mainContainer.append(boxContainer);
-
-    const title = document.createElement("h1");
-    title.textContent = data.title;
-    const subtitle = document.createElement("h5");
-    subtitle.textContent = data.subtitle;
-    const emoji = document.createElement("h2");
-    emoji.textContent = data.emoji;
-    header.classList = "colCentre";
-    header.append(title, subtitle, emoji);
+    // const mainContainer = document.createElement("div");
+    // mainContainer.classList = "colCentre";
+    // const boxContainer = document.createElement("div");
+    // boxContainer.classList = "colCentre leaderBox";
+    // mainContainer.append(boxContainer);
+    
+    const titleObj ={
+        htmlTag:"h1",
+        properties:{textContent: data.title},
+    };
+    const subtitleObj ={
+        htmlTag:"h4",
+        properties:{textContent: data.subtitle},
+    };
+    const emojiObj ={
+        htmlTag:"h2",
+        properties:{textContent: data.emoji},
+    };
+    const headerObj ={
+        htmlTag:"header",
+        tagClass:["colCentre"],
+        children:[titleObj, subtitleObj, emojiObj]
+    };
+    const header = templateFabrica(headerObj);
 
     const elementsBox = document.createElement("div");
     elementsBox.classList = "elsBox";
     data.users.map( (user, index) => {
         const elContainer = document.createElement("div");
         elContainer.classList = "elContainer";
-        const liderElement = document.createElement("div");
-        liderElement.classList = "liderElement colCentre";
 
-        const avatar = document.createElement("img");
-        avatar.src = user.avatar;
-        const name = document.createElement("p");
-        name.textContent = user.name;
-        const valueText = document.createElement("p");
-        valueText.textContent = user.valueText;
+        const avatar = {
+            htmlTag: "img",
+            properties:{src: user.avatar}
+        };
+ 
+        const name = {
+            htmlTag: "p",
+            properties:{textContent: user.name}
+        };
+        const valueText = {
+            htmlTag: "p",
+            properties:{textContent: user.valueText}
+        }; 
+
         const bar = document.createElement("div");
         let barClass = '';
         if(index === 0) {
@@ -36,11 +51,16 @@
         } else if(index <3) {
             barClass = "secondBar";
         } else {
-            barClass = "firdBar"
+            barClass = "thirdBar"
         }
         bar.classList = `${barClass} bar`;
 
-        liderElement.append(avatar, name, valueText, bar);
+        const liderElement = templateFabrica({
+            htmlTag: "div",
+            tagClass:["liderElement" ,"colCentre"],
+            children:[avatar, name, valueText, bar]
+        }); 
+    
         elContainer.append(liderElement);
         const sddMethod = index%2 === 0 ? "prepend" : "append";
         elementsBox[sddMethod](elContainer);
@@ -48,7 +68,17 @@
         return null;
     });
 
-    boxContainer.append(header, elementsBox);
+    const boxContainer = templateFabrica({
+        htmlTag: "div",
+        tagClass:["leaderBox" ,"colCentre"],
+        children:[header, elementsBox]
+    });
 
+    const mainContainer = templateFabrica({
+        htmlTag: "div",
+        tagClass:["colCentre"],
+        children:[boxContainer]
+    });
+   
     return mainContainer.innerHTML;
 }
---
+++ templates/templateFabrica.js
@@ -0,0 +1,22 @@
+// const shablon ={
+//     htmlTag: 'div',
+//     properties: {propName: value},
+//     tagClass:[],
+//     children: [shablons]
+// };
+
+const templateFabrica = (shablon) => {
+    if (!shablon.htmlTag) return shablon;
+    const element = document.createElement(shablon.htmlTag);
+    element.classList = shablon.tagClass?.join(' ');
+    if(shablon.properties){
+        for (const property in shablon.properties) {
+            element[property]=shablon.properties[property];
+        }
+    }
+    shablon.children?.forEach(chiled => {
+        element.append(templateFabrica(chiled));
+    });
+
+    return element;
+}
